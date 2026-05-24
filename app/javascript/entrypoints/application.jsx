ba3339dfde94a989c98740109d07392cad9cfd9a
Genevieve McAllister <genevieve@Genevieves-Computer.local>
2026-05-24 14:27:44 -0700

    hello world created

--- app/javascript/entrypoints/application.jsx
+++ app/javascript/entrypoints/application.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import App from '../App'
 
 const container = document.getElementById('root')
-if (container) {
-  const root = createRoot(container)
-  root.render(<React.StrictMode>{/* App goes here */}</React.StrictMode>)
-}
+const root = createRoot(container)
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
