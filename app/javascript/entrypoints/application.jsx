ad3e7bc22eafe205987e18c76502211512beb77b
Genevieve McAllister <genevieve@Genevieves-Computer.local>
2026-05-24 13:49:38 -0700

    Initial setup: ruby, rails, react, vite

---
+++ app/javascript/entrypoints/application.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+
+const container = document.getElementById('root')
+if (container) {
+  const root = createRoot(container)
+  root.render(<React.StrictMode>{/* App goes here */}</React.StrictMode>)
+}
---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import js from '@eslint/js'
+import reactPlugin from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import prettier from 'eslint-config-prettier'
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,jsx}'],
+    plugins: {
+      react: reactPlugin,
+      'react-hooks': reactHooks,
+      'jsx-a11y': jsxA11y,
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+      globals: {
+        window: 'readonly',
+        document: 'readonly',
+        console: 'readonly',
+      },
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+    rules: {
+      ...reactPlugin.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      ...jsxA11y.configs.recommended.rules,
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-console': 'warn',
+    },
+  },
+  prettier,
+]
